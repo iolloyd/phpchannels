a34b5c5310d46b2e46760d7702f17c30b73951cc
Lloyd Moore <manchesterboy@gmail.com>
2016-12-28 19:34:28 +0000

    init

--- test.php
+++ test.php
@@ -16,7 +16,7 @@ $routine->go('someFunc', $ch, 12345);
 
 $routine->go(function($x) use ($ch1) { 
     $ch1->write($a); 
-}, 100000);
+}, 67890);
 
 $routine->go(function() use ($ch1, $ch2) {
     list($a, $b) = [$ch->read(), $ch->read()];
